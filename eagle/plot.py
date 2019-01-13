ba8dc74122ac0bbf3da59e32e1644e122b435436
Eva van Weenen <evavanweenen@gmail.com>
2019-01-13 19:10:53 +0100

    Change colors to orange and blue and format MSE and R2 scores

--- eagle/plot.py
+++ eagle/plot.py
@@ -8,18 +8,21 @@ plt.style.use('seaborn-colorblind')
 
 dir = '/disks/strw9/vanweenen/mrp2/plots/'
 
+orange = '#FF6500'
+blue = '#009DFF'  ##33D6FF  
+
 def title(sim, cat, snapshot, score=None, MLtype=None, small=False, **kwargs):
     title = '' ; legendtitle = None
     if MLtype != None:
         title = MLtype
-        legendtitle = 'MSE = %s, $R^2$ = %s'%(np.around(score[0],3), np.around(score[1],3))
+        legendtitle = 'MSE = %.2g, $R^2$ = %.2g'%(score[0],score[1])
     plottitle = title + ' %s $\mid$ %s $\mid$ snapshot = %s \n'%(sim, cat, snapshot)
     savetitle = title + '_%s_%s_%s'%(sim, cat, snapshot)
-    for i in kwargs:
+    for count, i in enumerate(kwargs):
         savetitle += '_' + str(i) + '=' + str(kwargs[i])
         plottitle += str(i) + ' = ' + str(kwargs[i]) + ' $\mid$ '
-        if (small and count == 2):
-            plottitle += '\n'
+        #if (small == True and count%3 == 2):
+        #    plottitle += ' \n '
     return plottitle, savetitle, legendtitle
 
 def plot_sfr_z(sims, L, sfrs, zs):
@@ -38,18 +41,17 @@ def plot_sfr_z(sims, L, sfrs, zs):
 def plot_learning_curve(history, epochs, test='validation'):
     fig, ax = plt.subplots(2,1, squeeze=True, sharex=True)
     plt.suptitle('Learning rate')
-    ax[0].plot(np.arange(epochs), history.history['loss'], color='grey', label='train')
-    ax[0].plot(np.arange(epochs), history.history['val_loss'], color='red', label=test)
+    ax[0].plot(np.arange(epochs), history.history['loss'], color=blue, label='train')
+    ax[0].plot(np.arange(epochs), history.history['val_loss'], color=orange, label=test)
     ax[0].set_ylabel('MSE')
     ax[0].set_xlim((0., epochs))
-    ax[1].plot(np.arange(epochs), history.history['coeff_determination'], color='grey')
-    ax[1].plot(np.arange(epochs), history.history['val_coeff_determination'], color='red')
+    ax[1].plot(np.arange(epochs), history.history['coeff_determination'], color=blue)
+    ax[1].plot(np.arange(epochs), history.history['val_coeff_determination'], color=orange)
     ax[1].set_ylabel('$R^2$')
     ax[1].set_xlabel('epoch')
     ax[1].set_xlim((0., epochs))
     ax[0].legend()
     plt.savefig(dir+'Learning_rate_epochs%s.pdf'%epochs)
-    #plt.show()
 
 def plot_sed_mstar(x, y, dataset, sim, cat, snapshot, xnames, ynames, predict=True, y_pred=None, score=None, MLtype=None, **kwargs):
     plottitle, savetitle, legendtitle = title(sim, cat, snapshot, score, MLtype, small=False, **kwargs)
@@ -60,21 +62,22 @@ def plot_sed_mstar(x, y, dataset, sim, cat, snapshot, xnames, ynames, predict=Tr
     ax[0].set_ylabel(ynames[0])
     fig.suptitle(plottitle)
     for i, xname in enumerate(xnames):
-        ax[i].scatter(x[:,i], y, s=2., marker='o', label=dataset, color='dimgrey', edgecolor='none')
+        ax[i].plot(x[:,i], y, 'o', markersize=1., markerfacecolor=blue, markeredgecolor='none', label='target')
+        #ax[i].scatter(x[:,i], y, s=2., marker='o', label='target', color=blue, edgecolor='none')
         if predict:        
             idx = np.argsort(x[:,i])
             x_sort = x[:,i][idx]
             y_sort = y_pred[idx]
-            ax[i].scatter(x_sort, y_sort, marker='o', s=2., label='predict', color='red', edgecolor='none')      
+            ax[i].plot(x_sort, y_sort, 'o', markersize=1., markerfacecolor=orange, markeredgecolor='none', label='predict')
+            #ax[i].scatter(x_sort, y_sort, marker='o', s=2., label='predict', color=orange, edgecolor='none')      
         ax[i].set_xlabel(xname)
     plt.legend(title=legendtitle)
     plt.savefig(dir+savetitle)
-    #plt.show()
 
 
 def plot_prediction_test(y, y_pred, sim, cat, snapshot, score=None, MLtype='', **kwargs):
-    xname='log $M_{*}$'; yname='predicted log $M_{*}$'
-    plottitle, savetitle, legendtitle = title(sim, cat, snapshot, score, MLtype, small=False, **kwargs)
+    xname='target log $M_{*}$'; yname='predicted log $M_{*}$'
+    plottitle, savetitle, legendtitle = title(sim, cat, snapshot, score, MLtype, small=True, **kwargs)
     savetitle += '_test-predict.pdf'
     
     plt.figure(figsize=(8,6))
@@ -85,7 +88,6 @@ def plot_prediction_test(y, y_pred, sim, cat, snapshot, score=None, MLtype='', *
     plt.ylabel(yname)
     plt.legend(title=legendtitle)
     plt.savefig(dir+savetitle)
-    #plt.show()
        
 def gif_sed_mstar(x, y, Y_pred, dataset, sim, cat, snapshot, xnames, ynames, mse, r2, MLtype=None, **kwargs):
     plottitle, savetitle, legendtitle = title(sim, cat, snapshot, [mse[19],r2[19]], MLtype, small=False, **kwargs)
@@ -100,13 +102,13 @@ def gif_sed_mstar(x, y, Y_pred, dataset, sim, cat, snapshot, xnames, ynames, mse
 
     scatters = []
     for i, xname in enumerate(xnames):
-        ax[i].plot(x[:,i], y, 'o', markersize=1., markerfacecolor='dimgrey', markeredgecolor='none', label=dataset)        
+        ax[i].plot(x[:,i], y, 'o', markersize=1., markerfacecolor=blue, markeredgecolor='none', label='target')        
         ax[i].set_xlabel(xname)
 
         idx = np.argsort(x[:,i])
         x_sort = x[:,i][idx]
         y_sort = Y_pred[0][idx]
-        scatter, = ax[i].plot(x_sort, y_sort, 'o', markersize=1., markerfacecolor='red', markeredgecolor='none', label='predict')
+        scatter, = ax[i].plot(x_sort, y_sort, 'o', markersize=1., markerfacecolor=orange, markeredgecolor='none', label='predict')
         scatters.append(scatter)
     """
     def init():
@@ -119,9 +121,8 @@ def gif_sed_mstar(x, y, Y_pred, dataset, sim, cat, snapshot, xnames, ynames, mse
             idx = np.argsort(x[:,i])
             y_sort = Y_pred[n][idx]
             scatter.set_ydata(y_sort)
-        plt.legend(title= 'MSE = %s, $R^2$ = %s'%(np.around(mse[n],3), np.around(r2[n],3))+ ' epoch %s'%n)
+        plt.legend(title= 'MSE = %.2g, $R^2$ = %.2g'%(mse[n], r2[n])+ ' epoch %s'%n)
         return scatters
 
     anim = FuncAnimation(fig, update, fargs = (scatters, x, Y_pred, mse, r2), frames = np.arange(kwargs['epochs']), save_count=20, interval=1000)
     anim.save(dir+savetitle, dpi=300, writer=Writer(fps=1, metadata=dict(artist='Eva'), bitrate=1800))
-    plt.show()
