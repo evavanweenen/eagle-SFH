6fa94c57ca03e1a201876fdf0e499631bcecde6f
Eva van Weenen <evavanweenen@gmail.com>
2019-01-29 18:20:00 +0100

    Cosmetic change

--- read_starparticles.py
+++ read_starparticles.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import h5py 
 
+#---------------------------------------
 dir = '/disks/strw9/vanweenen/mrp2/'
 file = 'Ref100_histories.hdf5' 
 hf = h5py.File(dir+file, 'r')  
@@ -8,6 +9,4 @@ hf = h5py.File(dir+file, 'r')
 galid = hf['Galaxies/GalaxyID'] 
 histories = hf['Histories/SFR_vs_time'] 
 redshift = hf['Histories/Z_vs_time']
-
-
 
