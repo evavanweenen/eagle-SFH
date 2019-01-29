6c2acafe9d9cfdaa929e87bc9fa01179c7ca0f21
Eva van Weenen <evavanweenen@gmail.com>
2019-01-29 18:22:01 +0100

    Add compare with SDSS part, linear output activation function and a lot of changes, TODO: hyperas

--- mlp.py
+++ mlp.py
@@ -11,117 +11,109 @@ np.random.seed(seed) #fix seed for reproducibility
 fol = ''
 sim = 'RefL0100N1504' #simulation
 cat = 'dusty-sdss' #catalogue to build model to
-snap = 27 #redshift to use
-#L = 100 #box size
+snap = 27 #snapshot to use
+redshift = 0.1
 
 #input output
 inp = 'dusty-sdss'
 outp = 'mstar'#'sfr'
-xnames=['log dusty sdss ' + f for f in ['u','g','r','i','z']]
-ynames=['log $M_{*}$']#['log SFR']
-xcols = ['dusty_sdss_' + f for f in ['u','g','r','i','z']]
-ycols = ['m_star']#['sfr']
+xnames=['$\log_{10}$ dusty sdss flux ' + f + ' (Jy) ' for f in ['u','g','r','i','z']]
+ynames=['$\log_{10} M_{*} (M_{\odot})$']#['log SFR']
 
-#ML settings default
+#eagle
+eagle_xtype = 'flux'
+eagle_xcols = ['dusty' + eagle_xtype + '_sdss_' + f for f in ['u','g','r','i','z']]
+eagle_ycols = ['m_star']#['sfr']
+
+#sdss
+sdss_xtype = ''
+sdss_xcols = ['flux_' + f + sdss_xtype for f in ['u', 'g', 'r', 'i', 'z']]
+sdss_ycols = ['Mstellar_median']
+sdss_datacols = sdss_xcols + sdss_ycols
+
+#ML settings default (now for maximum of 3 hidden layers)
 perc_train = .8
-h_neurons = [30,30]
-dropout = [.5, .5]
-activation_h=['tanh', 'tanh']
-activation_o='relu'
+h_nodes = [30, 30, None]
+dropout = [.5, .5, None]
+activation = ['tanh', 'tanh', None, 'linear']
 loss = 'mean_squared_error'
 lr_rate = 0.001
 epochs = 20
 batch_size = 64#8
 optimizer = 'Adam'
 
-def preprocess(io, dtype = ['<i8','<i8','<f8','<f8','<f8','<f8','<f8','<f8','<f8', '<f8'], skip_header=15):
-    #read data     
-    io.read_data(dtype=dtype, skip_header=skip_header)
-
-    #divide data into x and y
-    x, y = io.divide_input_output()
-    
-    #scale data to a logarithmic scale and then scale linearly to values between 0 and 1
-    x = io.rescale_log(x)
-    y = io.rescale_log(y)
-    x, xscaler = io.rescale_lin(x)
-    y, yscaler = io.rescale_lin(y)
-
-    #divide data into train and test set
-    x_train, y_train, x_test, y_test = io.perm_train_test(x, y)
-
-    print("Total size of data: %s; size of training set: %s ; size of test set: %s"%(len(x), len(x_train), len(x_test)))
-    return x, y, x_train, y_train, x_test, y_test, xscaler, yscaler
-
-def postprocess(io, x_test, y_test, y_pred, xscaler, yscaler):
-    #return x_test, y_test and y_pred to their original scale    
-    x_test = io.invscale_lin(x_test, xscaler)
-    y_test = io.invscale_lin(y_test, yscaler)
-    y_pred = io.invscale_lin(y_pred, yscaler)
-    return x_test, y_test, y_pred
-
 
 #------------------------------------------DO NOT CREATE GIF--------------------------------------------------------------
 #read data
-io = IO(fol, sim, cat, snap, xcols, ycols, perc_train)
-x, y, x_train, y_train, x_test, y_test, xscaler, yscaler = preprocess(io)
+eagle = EAGLE(fol, sim, cat, snap, redshift, eagle_xcols, eagle_ycols, perc_train)
+x_train, y_train, x_test, y_test = eagle.preprocess(eagle_xtype)
 
 input_size = len(x_train[0])
 output_size = len(y_train[0])
-neurons = [input_size] + h_neurons + [output_size]
-print("Neurons in the network: ", neurons)
+nodes = [input_size] + h_nodes + [output_size]
+print("nodes in the network: ", nodes)
 
-#read hyperparameters and make architecture of the network
-nn = NN(input_size, output_size, h_neurons, activation_h, dropout, activation_o, loss, lr_rate, epochs, batch_size, optimizer)
+#read hyperparameters and make network
+nn = NN(input_size, output_size, h_nodes, activation, dropout, loss, lr_rate, epochs, batch_size, optimizer)
 nn.MLP_model()
 
-#train
-result = nn.model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=0, validation_data=(x_test, y_test))
-#evaluate
-score = nn.model.evaluate(x_test, y_test, verbose=0)
-#predict
-y_pred = nn.model.predict(x_test) 
-#postprocessing
-x_test, y_test, y_pred = postprocess(io, x_test, y_test, y_pred, xscaler, yscaler)
+#train, evaluate, predict, postprocess
+result = nn.model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=0, validation_data=(x_test, y_test)) #train
+score = nn.model.evaluate(x_test, y_test, verbose=0) #evaluate
+y_pred = nn.model.predict(x_test) #predict
+x_test, y_test, y_pred = eagle.postprocess(x_test, y_test, y_pred) #postprocessing
 
 #plot
-plot = Plot(io, nn, xnames, ynames, MLtype='MLP', score=score)#[mse[19], r2[19]]
-
-#plotting
+plot = PLOT_NN(eagle, nn, xnames, ynames, MLtype='MLP', score=score)#[mse[19], r2[19]]
 plot.plot_learning_curve(result, nn.epochs)
 plot.plot_input_output(x_test, y_test, y_pred)
 plot.plot_true_predict(y_test, y_pred)
 
-  
+
+#------------------------------------------Compare with SDSS--------------------------------------------------------------
+#read data
+sdss = SDSS(sdss_datacols, sdss_xcols, sdss_ycols, redshift = 0.1)
+sdss.preprocess(eagle)
+
+#plot data
+plot_data = PLOT_DATA(xnames+ynames, sim=sim, cat=cat, snap=snap)
+plot_data.hist_data(('eagle-preprocessed-minmax', 'sdss-preprocessed-minmax'), [np.hstack((eagle.x, eagle.y)), np.hstack((sdss.x, sdss.y))], xlim=[-1,2], ylim=[-2,1.5])
+
+#evaluate, predict, postprocess
+sdss_score = nn.model.evaluate(sdss.x, sdss.y, verbose=0) #evaluate
+sdss.ypred = nn.model.predict(sdss.x) #predict
+sdss.x, sdss.y, sdss.ypred = sdss.postprocess(eagle, sdss.x, sdss.y, sdss.ypred) #postprocessing
+
+#plot
+plot = PLOT_NN(eagle, nn, xnames, ynames, MLtype='MLPsdss', score=sdss_score)#[mse[19], r2[19]]
+plot.plot_input_output(sdss.x, sdss.y, sdss.ypred)
+plot.plot_true_predict(sdss.y, sdss.ypred)
+
+
 #------------------------------------------CREATE GIF--------------------------------------------------------------
 #read data
-io = IO(fol, sim, cat, snap, xcols, ycols, perc_train)
-x, y, x_train, y_train, x_test, y_test, xscaler, yscaler = preprocess(io)
+eagle = EAGLE(fol, sim, cat, snap, redshift, eagle_xcols, eagle_ycols, perc_train)
+x_train, y_train, x_test, y_test = eagle.preprocess(eagle_xtype)
 
 input_size = len(x_train[0])
 output_size = len(y_train[0])
-neurons = [input_size] + h_neurons + [output_size]
-print("Neurons in the network: ", neurons)
+nodes = [input_size] + h_nodes + [output_size]
+print("nodes in the network: ", nodes)
 
-#read hyperparameters and make architecture of the network
-nn = NN(input_size, output_size, h_neurons, activation_h, dropout, activation_o, loss, lr_rate, epochs, batch_size, optimizer)
+#read hyperparameters and make network
+nn = NN(input_size, output_size, h_nodes, activation, dropout, loss, lr_rate, epochs, batch_size, optimizer)
 nn.MLP_model()
 
-
-#train network
 Y_pred, mse, r2 = [], [], [] 
 for i in range(epochs):
-    #train    
-    result = nn.model.fit(x_train, y_train, batch_size=batch_size, epochs=1, verbose=0, validation_data=(x_test, y_test))
-    #evaluate    
-    score = nn.model.evaluate(x_test, y_test, verbose=0)
-    #predict
-    y_pred = nn.model.predict(x_test)
-    #postprocess    
-    x_test_log, y_test_log, y_pred = postprocess(io, x_test, y_test, y_pred, xscaler, yscaler)
-    #plot
-    plot = Plot(io, nn, xnames, ynames, MLtype='MLP', score=score, epochs=i)
-    plot.plot_input_output(x_test_log, y_test_log, y_pred)
+    #train, evaluate, predict, postprocess
+    result = nn.model.fit(x_train, y_train, batch_size=batch_size, epochs=1, verbose=0, validation_data=(x_test, y_test)) #train 
+    score = nn.model.evaluate(x_test, y_test, verbose=0) #evaluate
+    y_pred = nn.model.predict(x_test) #predict
+    x_test_log, y_test_log, y_pred = eagle.postprocess(x_test, y_test, y_pred) #postprocess
+    #plotting
+    plot = PLOT_NN(eagle, nn, xnames, ynames, MLtype='MLP', score=score, epochs=i)
+    #plot.plot_input_output(x_test_log, y_test_log, y_pred)
     #plot.plot_true_predict(y_test_log, y_pred)
     #save values for gif    
     Y_pred.append(y_pred) ; mse.append(score[0]) ; r2.append(score[1])
@@ -132,6 +124,19 @@ plot.gif_input_output(x_test_log, y_test_log, Y_pred=Y_pred, mse=mse, r2=r2)
 
 """
 #-------------------------------------------- Test model from hyperas -------------------------------------------------------
+#Hyperas
+perc_train = .8
+h_nodes = 3*[{{choice([5, 10, 20, 30, 40, 50, 60])}}]
+dropout = 3*[{{uniform(0,1)}}]
+activation = 3*[{{choice(['relu', 'tanh', 'sigmoid', 'linear'])}}]+[{{choice(['relu', 'linear'])}}]
+loss = 'mean_squared_error'
+#lr_rate = 0.001
+epochs = 20
+batch_size = {{choice([8, 16, 32, 64])}}
+optimizer = {{choice(['sgd', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam'])}}
+
+
+
 dir_hyp =  '/disks/strw9/vanweenen/mrp2/optimization/'
 hyp = '20190114A'
 
@@ -146,13 +151,13 @@ batch_size = 8
 optimizer = 'RMSprop'
 
 #read data
-io = IO(fol, sim, cat, snap, xcols, ycols, perc_train)
+io = EAGLE(fol, sim, cat, snap, redshift, eagle_xcols, eagle_ycols, perc_train)
 x, y, x_train, y_train, x_test, y_test, xscaler, yscaler = preprocess(io)
 
 input_size = len(x_train[0])
 output_size = len(y_train[0])
-neurons = [input_size] + h_neurons + [output_size]
-print("Neurons in the network: ", neurons)
+nodes = [input_size] + h_nodes + [output_size]
+print("nodes in the network: ", nodes)
 
 #evaluate
 score = model.evaluate(x_test, y_test, verbose=0)
