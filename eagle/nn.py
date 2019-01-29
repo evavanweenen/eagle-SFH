7c54d2fcbafffa9ebb63f09714d578fadb330c6e
Eva van Weenen <evavanweenen@gmail.com>
2019-01-29 18:16:26 +0100

    Add GridSearchCV

--- eagle/nn.py
+++ eagle/nn.py
@@ -4,69 +4,110 @@ from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Activation
 from keras.optimizers import RMSprop, Adam, SGD
 from sklearn.model_selection import GridSearchCV
-from keras.wrappers.scikit_learn import KerasClassifier
+from keras.wrappers.scikit_learn import KerasRegressor
 
 
-class NN():
-    def __init__(self, input_size, output_size, h_neurons, activation_h, dropout, activation_o, loss, lr_rate, epochs, batch_size, optimizer):
+class NN:
+    def __init__(self, input_size, output_size, h_nodes, activation, dropout, loss, lr_rate, epochs, batch_size, optimizer):
         self.input_size = input_size
         self.output_size = output_size
-        self.h_neurons = h_neurons
-        self.activation_h = activation_h
+        self.h_nodes = h_nodes
+        self.activation = activation
         self.dropout = dropout
-        self.activation_o = activation_o
+        self.h_nodes0 = self.h_nodes[0] ; self.activation0 = self.activation[0] ; self.dropout0 = self.dropout[0]
+        self.h_nodes1 = self.h_nodes[1] ; self.activation1 = self.activation[1] ; self.dropout1 = self.dropout[1]
+        self.h_nodes2 = self.h_nodes[2] ; self.activation2 = self.activation[2] ; self.dropout2 = self.dropout[2]
+        self.activation_out = self.activation[3]
         self.loss = loss
         self.lr_rate = lr_rate
         self.epochs = epochs
         self.batch_size = batch_size
         self.optimizer = optimizer
-            
-    def coeff_determination(self, y_true, y_pred):
-        """
-        Determination coefficient metric. Add K.epsilon()=1e-8 to avoid division by zero.
-        Arguments
-            y_true  array of true values of y
-            y_pred  array of predicted values of y
-        Returns
-            R2  determination coefficient
-        """
-        SSE = K.sum(K.square(y_true - y_pred))
-        TSS = K.sum(K.square(y_true - K.mean(y_true)))
-        return 1-SSE/(TSS+K.epsilon())
-    
+        
+ 
     def MLP_model(self):
         """
         Build a sequential NN with input size 'in_size', output size 'out_size', 
         number of hidden layers 'len(neurons)-2' and number of neurons per layer i 'neurons[i]'
         """
+        """
+        h_nodes0 = self.h_nodes[0] ; activation0 = self.activation[0] ; dropout0 = self.dropout[0]
+        h_nodes1 = self.h_nodes[1] ; activation1 = self.activation[1] ; dropout1 = self.dropout[1]
+        h_nodes2 = self.h_nodes[2] ; activation2 = self.activation[2] ; dropout2 = self.dropout[2]
+        activation_out = self.activation[3]
+        """
         print("Building model..")
-        # first hidden layer
         self.model = Sequential()
-        self.model.add(Dense(self.h_neurons[0], input_dim=self.input_size, use_bias=True))
-        self.model.add(Activation(self.activation_h[0]))
-        self.model.add(Dropout(self.dropout[0]))
-        # rest of hidden layers
-        for i in range(1, len(self.h_neurons)):
-            self.model.add(Dense(self.h_neurons[i], use_bias=True))
-            self.model.add(Activation(self.activation_h[i]))
-            self.model.add(Dropout(self.dropout[i]))
+
+        # first hidden layer (0)
+        self.model.add(Dense(self.h_nodes0, input_dim=self.input_size, use_bias=True))
+        self.model.add(Activation(self.activation0))
+        self.model.add(Dropout(self.dropout0))
+
+        # second hidden layer (1)
+        if self.h_nodes1 != None:
+            self.model.add(Dense(self.h_nodes1, use_bias=True))
+            self.model.add(Activation(self.activation1))
+            self.model.add(Dropout(self.dropout1))
+
+        # third hidden layer (2)
+        if self.h_nodes2 != None:
+            self.model.add(Dense(self.h_nodes2, use_bias=True))
+            self.model.add(Activation(self.activation2))
+            self.model.add(Dropout(self.dropout2))
+
         #output layer
         self.model.add(Dense(self.output_size))
-        self.model.add(Activation(self.activation_o))
-        #compile model
-        self.model.compile(loss=self.loss, optimizer=self.optimizer, metrics=[self.coeff_determination])
-        return self.model   
+        self.model.add(Activation(self.activation_out))
 
-    def GridSearch(self):
+        #compile model
+        self.model.compile(loss=self.loss, optimizer=self.optimizer, metrics=[coeff_determination])
+
+        return self.model
+
+    def sklearn_wrapper(self, h_nodes0, activation0, h_nodes1, activation1, h_nodes2, activation2):
+        self.h_nodes0 = h_nodes0 ; self.activation0 = activation0 #; self.dropout0 = dropout0
+        self.h_nodes1 = h_nodes1 ; self.activation1 = activation1 #; self.dropout1 = dropout1
+        self.h_nodes2 = h_nodes2 ; self.activation2 = activation2 #; self.dropout2 = dropout2    
+        return self.MLP_model()
+
+    def GridSearch(self, x, y, folds=5):
         #TODO: checken of self.model returnen nodig is bij KerasClassifier
         #https://blogs.oracle.com/meena/simple-neural-network-model-using-keras-and-grid-search-hyperparameterstuning
         #TODO: finish grid search
-        model = KerasClassifier(build_fn=self.MLP_model, verbose=0)
-        param_grid = dict(h_neurons=self.h_neurons, dropout=self.dropout, activation_h=self.activation_h, activation_o=self.activation_o, batch_size=self.batch_size, optimizer=self.optimizer)        
-        self.grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, n_jobs=-1)
+        model = KerasRegressor(build_fn=self.sklearn_wrapper, verbose=0)
+        param_grid = dict(  h_nodes0 = self.h_nodes[0], activation0 = self.activation[0], \
+                            h_nodes1 = self.h_nodes[1], activation1 = self.activation[1], \
+                            h_nodes2 = self.h_nodes[2], activation2 = self.activation[2], )
+        self.grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring='neg_'+self.loss, cv=folds, verbose=1, n_jobs=2)
+
+        self.grid_result = self.grid.fit(x, y)
+
+        print("Best: %f using %s" %(self.grid_result.best_score_, self.grid_result.best_params_))
+        self.means = self.grid_result.cv_results_['mean_test_score']
+        self.stds = self.grid_result.cv_results_['std_test_score']
+        self.params = self.grid_result.cv_results_['params']
+        for mean, std, param in zip(self.means, self.stds, self.params):
+            print("%f (%f) with: %r" %(mean, std, param))
 
     def hyperas_create_model(self, X_train, Y_train, X_val, Y_val):
         self.MLP_model()
+
+
+def coeff_determination(y_true, y_pred):
+    """
+    Determination coefficient metric. Add K.epsilon()=1e-8 to avoid division by zero.
+    Arguments
+        y_true  array of true values of y
+        y_pred  array of predicted values of y
+    Returns
+        R2  determination coefficient
+    """
+    SSE = K.sum(K.square(y_true - y_pred))
+    TSS = K.sum(K.square(y_true - K.mean(y_true)))
+    return 1-SSE/(TSS+K.epsilon())
+
+
 
 """        
 class OPTIM(NN):
