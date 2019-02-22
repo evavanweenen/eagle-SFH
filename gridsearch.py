fc880e55dade3ba21c07c293030d7e83ddfd7b9a
Eva van Weenen <evavanweenen@gmail.com>
2019-02-22 22:23:26 +0100

    Perform gridsearch, save results in npy file, print top3 for mse and r2, add colors to input, add tensorflow random seed, add uniform mass distribution, add beginning of randomized search, move plot to eagle/plot

--- gridsearch.py
+++ gridsearch.py
@@ -3,56 +3,132 @@ from eagle.plot import *
 from eagle.nn import *
 
 import numpy as np
+import scipy as sp
 from scipy.sparse import diags
 
 import datetime
 import sys
+import tensorflow as tf
 
 seed = 7
 np.random.seed(seed) #fix seed for reproducibility
+tf.set_random_seed(seed)
 
-#EAGLE settings
-fol = ''
-sim = 'RefL0100N1504' #simulation
-cat = 'dusty-sdss' #catalogue to build model to
-snap = 27 #snapshot to use
-redshift = 0.1
-
-#input output
-inp = 'dusty-sdss'
-outp = 'mstar'#'sfr'
-xnames=['$\log_{10}$ dusty sdss flux ' + f + ' (Jy) ' for f in ['u','g','r','i','z']]
-ynames=['$\log_{10} M_{*} (M_{\odot})$']#['log SFR']
-
-#eagle
-eagle_xtype = 'flux'
-eagle_xcols = ['dusty' + eagle_xtype + '_sdss_' + f for f in ['u','g','r','i','z']]
-eagle_ycols = ['m_star']#['sfr']
-
-#sdss
-sdss_xcols = ['flux_' + f for f in ['u', 'g', 'r', 'i', 'z']]
-sdss_ycols = ['Mstellar_median']
-sdss_datacols = sdss_xcols + sdss_ycols + ['Redshift']
-
-#------------------------------------------GridSearch--------------------------------------------------------------
-#write output terminal to file
 path =  '/disks/strw9/vanweenen/mrp2/optimization/'
 name = ''
-
+"""
+#write output terminal to file
+name = ''
 date = datetime.date.today().strftime('%Y%m%d')
 f = open(path+'gridsearch_'+date+name+'.out', 'w')
 sys.stdout = f
+"""
 
+#EAGLE settings
+fol = ''
+sim = 'RefL0100N1504'   # simulation
+cat = 'sdss'            # catalogue to build model to
+dust = 'dusty'          # '' if no dust
+snap = 27               # snapshot to use
+redshift = 0.1          # redshift of snapshot    
+
+fluxes = ('u', 'g', 'r', 'i', 'z')
+colors = ()#('ug', 'gr', 'ri', 'iz')
+xcols = fluxes + colors
+
+#eagle
+eagle_xtype = 'flux'
+eagle_xcols = [dust + eagle_xtype + '_' + cat + '_' + f for f in xcols]
+eagle_ycols = ['m_star']#['sfr']#
+
+#sdss
+sdss_xtype = 'flux'
+sdss_xcols = [sdss_xtype + '_' + f for f in xcols]
+sdss_ycols = ['Mstellar_median']#['SFR_median']#
+
+#input output plot
+xnames=[dust + ' ' + cat + ' ' + i for i in list(fluxes) + [c[0] + '-' + c[1] for c in colors]]
+ynames=['$\log_{10} M_{*} (M_{\odot})$']#['$\log_{10}$ SFR $(M_{\odot} yr^{-1})$']#
+
+equal_bins = True #uniform mass distribution
+
+
+#------------------------------------------GridSearch--------------------------------------------------------------
 #hyperparams
 perc_train = .8
-h_nodes = [[20,40]]+2*[[None, 20, 40]]#h_nodes = 3*[[5, 10, 20, 30, 40, 50, 60]]
-dropout = [0., 0., 0.]#3*[[0., .4, .8]]#dropout = 3*[[0, 0.2, 0.4, 0.6, 0.8, 1.]]
+h_nodes = [20, 50, 50]#[[10, 20, 30, 40, 50]]+2*[[None, 10, 20, 30, 40, 50]]#
+dropout = [0, 0, 0]#3*[[0., 0.2, 0.4, 0.7]]#
+activation = ['tanh', 'linear', 'tanh', 'linear']#3*[['sigmoid', 'tanh', 'relu', 'linear']] + ['linear']#
+loss = 'mean_squared_error'
+lr_rate = 0.001
+epochs = 15
+batch_size = 128
+optimizer = ['sgd', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']#'Adam'#
+
+#read data
+eagle = EAGLE(fol, sim, cat, dust, snap, redshift)
+eagle.read_data()
+x_train, y_train, x_test, y_test = eagle.preprocess(colors, eagle_xcols, eagle_ycols, eagle_xtype, equal_bins)
+
+input_size = len(x_train[0])
+output_size = len(y_train[0])
+nodes = [input_size] + h_nodes + [output_size]
+print("nodes in the network: ", nodes)
+
+#read hyperparameters and make architecture of the network
+nn = NN(input_size, output_size, h_nodes, activation, dropout, loss, lr_rate, epochs, batch_size, optimizer)
+nn.GridSearch(x_train, y_train, 3)
+
+#save output
+grid_edges = optimizer #param grid: list with lists of options
+grid_names = ['optimizer'] #make sure this name is latex compatible
+grid_shape = [len(i) for i in grid_edges]
+
+grid_mse = nn.grid_result.cv_results_['mean_test_neg_mean_squared_error'].reshape(grid_shape) * -1
+grid_r2 = nn.grid_result.cv_results_['mean_test_r2'].reshape(grid_shape)
+
+np.save(path + 'mse' + str(grid_names) + name + '.npy', grid_mse)
+np.save(path + 'r2' + str(grid_names) + name + '.npy', grid_r2)
+
+"""
+grid_mse = np.load(path+'mse'+str(grid_names)+ name + '.npy')
+grid_r2 = np.load(path+'r2'+str(grid_names)+ name + '.npy')
+"""
+
+print("MSE: Best models")
+idxs_mse = np.argsort(grid_mse, axis=None)[:3]
+for m in range(3):
+    idx_mse = np.unravel_index(idxs_mse[m], grid_mse.shape)
+    print("number ", m,  'with mse: %.4e r2: %.4f'%(grid_mse[idx_mse], grid_r2[idx_mse]))
+    for i, idx in enumerate(idx_mse):
+        print(grid_names[i], grid_edges[i][idx])
+    
+
+print("r2: Best models")
+idxs_r2 = np.argsort(grid_r2, axis=None)[::-1][:3]
+for m in range(3):
+    idx_r2 = np.unravel_index(idxs_r2[m], grid_r2.shape)
+    print("number ", m,  'with mse: %.4e r2: %.4f'%(grid_mse[idx_r2], grid_r2[idx_r2]))
+    for i, idx in enumerate(idx_r2):
+        print(grid_names[i], grid_edges[i][idx])
+
+#plotting
+plot_gridsearch('mse', grid_mse, grid_edges, grid_names, name)
+plot_gridsearch('r2', grid_r2, grid_edges, grid_names, name)
+
+
+"""
+#------------------------------------------RandomizedSearch--------------------------------------------------------------
+#hyperparams
+perc_train = .8
+h_nodes = [[10, 20, 30, 40, 50]] + 2*[[None, 10, 20, 30, 40, 50]]
+dropout = 3*[sp.stats.uniform(0., 1.)]
 activation = 3*[['sigmoid', 'tanh', 'relu']]+['linear']#'relu', 'sigmoid', 'linear'
 loss = 'mean_squared_error'
 lr_rate = 0.001
 epochs = 20
 batch_size = 64
-optimizer = 'Adam'#['sgd', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']
+optimizer = ['sgd', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']
 
 #read data
 eagle = EAGLE(fol, sim, cat, snap, redshift, eagle_xcols, eagle_ycols, perc_train)
@@ -65,46 +141,10 @@ print("nodes in the network: ", nodes)
 
 #read hyperparameters and make architecture of the network
 nn = NN(input_size, output_size, h_nodes, activation, dropout, loss, lr_rate, epochs, batch_size, optimizer)
-nn.GridSearch(x_train, y_train, 2)
+nn.GridSearch(x_train, y_train, 2, n_iter=10)
 
-#save output
-grid_labels = ['h_nodes0', 'activation0', 'h_nodes1', 'activation1', 'h_nodes2', 'activation2']
-grid_names = ['h nodes0', 'activation0', 'h nodes1', 'activation1', 'h nodes2', 'activation2']
-grid_edges = [nn.h_nodes0, nn.activation0, nn.h_nodes1, nn.activation1, nn.h_nodes2, nn.activation2]
+"""
 
-grid_params = [nn.grid_result.cv_results_['param_'+i].filled() for i in grid_labels]
 
-grid_shape = [len(i) for i in grid_edges]
 
-grid_values = nn.means.reshape(grid_shape)
 
-np.save(path + 'mse.npy', grid_values)
-
-#plotting
-dim = len(grid_labels)
-
-fig, ax = plt.subplots(dim, dim, figsize=(7,6), squeeze=True)#, sharex=True, sharey=True)
-vmin = np.amin(grid_values) ; vmax = np.amax(grid_values)
-for i in range(dim):
-    for j in range(dim):
-        axis = tuple([x for x in np.arange(dim) if x!=i and x!=j])
-        if i == j:
-            diag = np.diagflat(np.average(grid_values, axis=axis))
-            diag[diag == 0] = np.nan
-            im = ax[i,j].imshow(diag, vmin=vmin, vmax=vmax, cmap='viridis', aspect='auto')
-        if i < j:
-            im = ax[i,j].imshow(np.average(grid_values, axis=axis), vmin=vmin, vmax=vmax, cmap='viridis', aspect='auto')
-        elif j < i:
-            im = ax[i,j].imshow(np.average(grid_values, axis=axis).T, vmin=vmin, vmax=vmax, cmap='viridis', aspect='auto')        
-        ax[i,j].set_xticks([])
-        ax[i,j].set_yticks([])        
-        ax[dim-1,j].set_xticks(np.arange(len(grid_edges[j])))
-        ax[dim-1,j].set_xticklabels(np.array(grid_edges[j]).astype('str').tolist())
-        ax[dim-1,j].set_xlabel(grid_names[j])
-    ax[i,0].set_ylabel(grid_names[i])
-    ax[i,0].set_yticks(np.arange(len(grid_edges[i])))  
-    ax[i,0].set_yticklabels(np.array(grid_edges[i]).astype('str').tolist())    
-cbar = fig.colorbar(im, ax=ax)
-cbar.ax.set_title('average MSE')
-plt.savefig(path+'GridSearch.pdf')
-plt.show()
